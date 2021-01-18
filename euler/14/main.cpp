63b9b2391fa2d7484c5ffec89031ae156cf52099
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-18 18:26:58 +0300

    moar

---
+++ euler/14/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstddef>
+
+static size_t collatz_len(unsigned n) {
+    if (n == 1) {
+        return 1;
+    }
+
+    if (n % 2 == 0) {
+        return 1 + collatz_len(n / 2);
+    }
+
+    return 1 + collatz_len(3 * n + 1);
+}
+
+int main() {
+    std::cout << collatz_len(13) << std::endl;
+    size_t cl = 0;
+    unsigned v = 0;
+
+    for (unsigned i = 1; i < 1000000; ++i) {
+        auto l = collatz_len(i);
+
+        if (l > cl) {
+            cl = l;
+            v = i;
+
+            std::cout << v << " " << cl << std::endl;
+        }
+    }
+}
