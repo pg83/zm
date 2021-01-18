d034cc6b52a99a95c52ff2a3c667d59008c7218f
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-18 04:24:31 +0300

    add

---
+++ euler/10/main.cpp
@@ -0,0 +1,15 @@
+#include <euler/lib/primes.h>
+
+#include <iostream>
+
+int main() {
+    size_t sum = 0;
+
+    for (size_t i = 2; i < 2000000; ++i) {
+        if (is_prime_stupid(i)) {
+            sum += i;
+        }
+    }
+
+    std::cout << sum << std::endl;
+}
---
+++ euler/9/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    for (size_t a = 1; a < 1000; ++a) {
+        for (size_t b = 1; a + b < 1000; ++b) {
+            size_t c = 1000 - a - b;
+
+            if (a * a + b * b == c * c) {
+                std::cout << a * b * c << std::endl;
+            }
+        }
+    }
+}
