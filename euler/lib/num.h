520cdfa8b45452ab31edc71121b5ffe8463a4dd5
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-20 23:46:29 +0300

    faster

--- euler/lib/num.h
+++ euler/lib/num.h
@@ -3,12 +3,18 @@
 #include <cstddef>
 
 template <class T>
-T pow_int(T x, size_t n) noexcept {
+T pow_int(T a, size_t pow) noexcept {
     T res = T(1);
+    T cur = a;
 
-    while (n) {
-        res *= x;
-        --n;
+    while (pow > 0) {
+        if (pow % 2 == 0) {
+            cur = cur * cur;
+            pow /= 2;
+        } else {
+            res = res * cur;
+            pow -= 1;
+        }
     }
 
     return res;
