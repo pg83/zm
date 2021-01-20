2ba39d798f417229034b85bef315f9e8711a5da8
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-20 23:39:11 +0300

    moar

--- euler/29/main.cpp
+++ euler/29/main.cpp
@@ -7,7 +7,7 @@
 int main() {
     std::set<bigint_t> res;
 
-    for (bigint_t a = 2; a <= 100; ++a) {
+    for (bigint_t a = 2, maxa = 100; a <= maxa; ++a) {
         for (int b = 2; b <= 100; ++b) {
             res.insert(pow_int(a, b));
         }
