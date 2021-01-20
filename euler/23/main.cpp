0920c1d0d7c3d9f065f47fd8e16dea37d87552ab
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-20 19:44:43 +0300

    moar

--- euler/23/main.cpp
+++ euler/23/main.cpp
@@ -1,2 +1,32 @@
+#include <euler/lib/primes.h>
+
+#include <unordered_set>
+#include <vector>
+#include <numeric>
+#include <iostream>
+
+template <class T>
+bool is_abundand(T t) {
+    return proper_divisors_sum(t) > t;
+}
+
 int main() {
+    std::vector<int> ab;
+    std::unordered_set<int> result;
+
+    for (int i = 1; i <= 28123; ++i) {
+        if (is_abundand(i)) {
+            ab.push_back(i);
+        }
+
+        result.insert(i);
+    }
+
+    for (auto x : ab) {
+        for (auto y : ab) {
+            result.erase(x + y);
+        }
+    }
+
+    std::cout << std::accumulate(result.begin(), result.end(), 0) << std::endl;
 }
