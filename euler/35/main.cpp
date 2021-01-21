27b8c8a38d7fe41f70e8c2254b64b010054dd800
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-21 18:06:32 +0300

    moar

---
+++ euler/35/main.cpp
@@ -0,0 +1,48 @@
+#include <euler/lib/num.h>
+#include <euler/lib/primes.h>
+
+#include <cstddef>
+#include <iostream>
+
+template <class T>
+size_t num_digits(T n) noexcept {
+    if (n == 0) {
+        return 1;
+    }
+
+    size_t res = 0;
+
+    while (n) {
+        ++res;
+        n /= 10;
+    }
+
+    return res;
+}
+
+static bool is_circular(int n) noexcept {
+    auto d = num_digits(n);
+    auto pd = pow_int(10, d - 1);
+
+    for (int i = 0; i < d; ++i) {
+        n = (n % 10) * pd + n / 10;
+
+        if (!is_prime_stupid(n)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    size_t res = 0;
+
+    for (int i = 2; i < 1000000; ++i) {
+        if (is_circular(i)) {
+            ++res;
+        }
+    }
+
+    std::cout << res << std::endl;
+}
