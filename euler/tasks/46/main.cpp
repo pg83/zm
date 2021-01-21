53efd6fb9cc449af9df5b94890e8bab8b4df860e
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-21 23:20:48 +0300

    moar

---
+++ euler/tasks/46/main.cpp
@@ -0,0 +1,36 @@
+#include <euler/lib/primes.h>
+
+#include <set>
+#include <iostream>
+
+int main() {
+    std::set<int> res;
+    auto n = 1000000;
+
+    for (auto i = 0; i < n; ++i) {
+        if (is_prime_stupid(i)) {
+            auto x = 0;
+            auto j = 1;
+
+            do {
+                x = i + 2 * j * j;
+                ++j;
+                res.insert(x);
+            } while (x <= n);
+        }
+    }
+
+    for (auto i = 1; i < n / 2; ++i) {
+        auto j = i * 2 + 1;
+
+        if (is_prime_stupid(j)) {
+            continue;
+        }
+
+        if (res.find(j) == res.end()) {
+            std::cout << j << std::endl;
+
+            break;
+        }
+    }
+}
