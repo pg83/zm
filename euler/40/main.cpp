223f4398cd9c0bd769a2dde9fc3e0d403df4fdd6
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-21 20:16:56 +0300

    moar

---
+++ euler/40/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string tmp;
+
+    for (int i = 0; tmp.length() < 2000000; ++i) {
+        tmp += std::to_string(i);
+    }
+
+    auto c = [&] (size_t n) -> int {
+        return tmp[n] - '0';
+    };
+
+    std::cout << c(1) * c(10) * c(100) * c(1000) * c(10000) * c(100000) * c(1000000) << std::endl;
+}
