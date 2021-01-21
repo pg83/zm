4ae7f6e97282742090c34cf40f12a335e802e325
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-22 01:11:07 +0300

    moar

--- euler/tasks/50/main.cpp
+++ euler/tasks/50/main.cpp
@@ -1,2 +1,44 @@
+#include <euler/lib/primes.h>
+
+#include <vector>
+#include <iostream>
+
 int main() {
+    long s = 0;
+    std::vector<long> sums;
+
+    for (long i = 0; i < 100000; ++i) {
+        if (is_prime_stupid(i)) {
+            s += i;
+            sums.push_back(s);
+        }
+    }
+
+    long maxd = 0;
+    long maxp = 0;
+
+    for (size_t i = 0; i < sums.size(); ++i) {
+        for (size_t j = 0; j < sums.size(); ++j) {
+            if (i > j) {
+                auto ip = sums[i];
+                auto jp = sums[j];
+
+                auto d = i - j;
+                auto p = ip - jp;
+
+                if (p < 1000000) {
+                    if (d > maxd) {
+                        if (is_prime_stupid(p)) {
+                            maxd = d;
+                            maxp = p;
+
+                            std::cout << maxd << " " << maxp << std::endl;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    std::cout << maxp << std::endl;
 }
