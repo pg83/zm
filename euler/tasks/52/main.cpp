e8a0e7fcdd5d4edd6e4f59ea344791f631a932c3
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-22 14:48:51 +0300

    cleanups

--- euler/tasks/52/main.cpp
+++ euler/tasks/52/main.cpp
@@ -1,14 +1,15 @@
 #include <euler/lib/algo.h>
+#include <euler/lib/types.h>
 
 #include <string>
 #include <iostream>
 
-static std::string keyf(int n) {
+static std::string keyf(uint n) {
     return sorted(std::to_string(n));
 }
 
 int main() {
-    for (int i = 1; ; ++i) {
+    for (uint i = 1; ; ++i) {
         auto k = keyf(i);
 
         if (k == keyf(2 * i)) {
