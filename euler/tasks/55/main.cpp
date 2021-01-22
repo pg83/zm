4a10dc3e1411337996e4b008b98252455d2fc830
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-22 15:12:15 +0300

    moar

---
+++ euler/tasks/55/main.cpp
@@ -0,0 +1,34 @@
+#include <euler/lib/bigint.h>
+
+#include <iostream>
+
+static bool do_check(bigint_t v, bigint_t vr, size_t c) {
+    if (c > 50) {
+        return true;
+    }
+
+    auto n = v + vr;
+    auto r = n.reverse();
+
+    if (n == r) {
+        return false;
+    }
+
+    return do_check(n, r, c + 1);
+}
+
+static bool check(bigint_t v) {
+    return do_check(v, v.reverse(), 0);
+}
+
+int main() {
+    uint res = 0;
+
+    for (uint i = 1; i < 10000; ++i) {
+        if (check(i)) {
+            ++res;
+        }
+    }
+
+    std::cout << res << std::endl;
+}
