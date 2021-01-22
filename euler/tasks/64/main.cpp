b8fc20f04df3f5dca42eada6d7950d6e870b0ad4
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-23 00:26:40 +0300

    moar

--- euler/tasks/64/main.cpp
+++ euler/tasks/64/main.cpp
@@ -1,5 +1,21 @@
 #include <euler/lib/euler.h>
 
 int main() {
-    std::cout << qir_t<int>::root_of(13).cont_fraction() << std::endl;
+    std::set<int> pows;
+
+    for (size_t i = 0; i <= 100; ++i) {
+        pows.insert(i * i);
+    }
+
+    size_t res = 0;
+
+    for (size_t i = 2; i <= 10000; ++i) {
+        if (pows.find(i) == pows.end()) {
+            if (qir_t<size_t>::root_of(i).cont_fraction().size() % 2 == 0) {
+                ++res;
+            }
+        }
+    }
+
+    std::cout << res << std::endl;
 }
