2b36ccbfd804f97531d6f8bf9b135e7a14d212c5
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-24 00:15:00 +0300

    moar

--- euler/tasks/71/main.cpp
+++ euler/tasks/71/main.cpp
@@ -3,8 +3,15 @@
 int main() {
     auto max = ratio_t(3, 7);
     auto prev = ratio_t(0);
+    ui64 cnt = 0;
 
     for (auto it : farey_seq(1000000)) {
+        ++cnt;
+
+        if (cnt % 1000000000 == 0) {
+            std::cout << cnt << std::endl;
+        }
+
         if (it < max) {
             prev = it;
         } else {
