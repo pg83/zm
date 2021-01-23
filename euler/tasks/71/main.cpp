16f321a6e95f7d4cdbdf988d002a06ddcae65275
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-24 01:12:49 +0300

    moar

--- euler/tasks/71/main.cpp
+++ euler/tasks/71/main.cpp
@@ -1,23 +1,22 @@
 #include <euler/lib/euler.h>
 
-int main() {
-    auto max = ratio_t(3, 7);
-    auto prev = ratio_t(0);
-    ui64 cnt = 0;
+template <class T>
+ratio_t<T> left_nearest(ratio_t<T> v, T maxdiv) {
+    ratio_t<T> res(0);
 
-    for (auto it : farey_seq(1000000)) {
-        ++cnt;
-
-        if (cnt % 1000000000 == 0) {
-            std::cout << cnt << std::endl;
-        }
-
-        if (it < max) {
-            prev = it;
-        } else {
-            break;
-        }
+    while (res.b < maxdiv) {
+        res.a += v.a;
+        res.b += v.b;
     }
 
-    std::cout << prev.a << std::endl;
+    while (res.b > maxdiv) {
+        res.a -= v.a;
+        res.b -= v.b;
+    }
+
+    return res;
+}
+
+int main() {
+    std::cout << left_nearest(ratio_t(3, 7), 1000000).a << std::endl;
 }
