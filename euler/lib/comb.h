e9aa67d554238eb5e6a0bff05537aab00a04f87c
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-01-24 20:30:48 +0300

    better

--- euler/lib/comb.h
+++ euler/lib/comb.h
@@ -43,7 +43,7 @@ auto eval_pq(F&& cf) {
 
 template <class T>
 auto partition_counter() {
-    auto pc = memoized ([zero = T(int(0)), one = T(int(1))](auto& pc, long n) -> T {
+    auto pc = memoized([zero = T(int(0)), one = T(int(1))](auto& pc, long n) -> T {
         if (n < 0) {
             return zero;
         }
