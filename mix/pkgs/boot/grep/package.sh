22e97e7db806fb95a27bc31fa70c6d9968a243f4
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-07-02 04:35:43 +0300

    add

---
+++ mix/pkgs/boot/grep/package.sh
@@ -0,0 +1,7 @@
+# url https://ftp.gnu.org/gnu/grep/grep-3.6.tar.xz
+# md5 f47fe27049510b2249dba7f862ac1b51
+# build-depends boot/sed boot/make boot/coreutils boot/heirloom boot/bin/stdenv
+
+build() {
+    {% include 'build.sh' %}
+}
