10d30cf23cb6d3f61f27f5683cca0ea653c7080a
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-07-02 04:44:58 +0300

    add

---
+++ mix/pkgs/boot/make/package.sh
@@ -0,0 +1,7 @@
+# url https://ftp.gnu.org/gnu/make/make-4.3.tar.gz
+# md5 fc7a67ea86ace13195b0bce683fd4469
+# build-depends boot/heirloom boot/bmake boot/bin/stdenv
+
+build() {
+    {% include 'build.sh' %}
+}
